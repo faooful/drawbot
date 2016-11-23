6cb930b4ec8daa7eebe704c1e13ca2e075bde428
faooful <faooful@gmail.com>
2016-11-23 18:18:32 +0000

    messin

--- ebooks.py
+++ ebooks.py
@@ -95,7 +95,7 @@ if __name__=="__main__":
         #     else:
         #         tweet+="."
         #     mine.add_text(tweet)
-
+        #
         # for x in range(0,10):
         #     ebook_tweet = mine.generate_sentence()
 
@@ -106,35 +106,35 @@ if __name__=="__main__":
         word_two = source_tweets[random.randrange(source_length)]
         ebook_tweet = " ".join([word_one, word_two])
 
-        # #randomly drop the last word, as Horse_ebooks appears to do.
-        # if random.randint(0,4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
-        #    print "Losing last word randomly"
-        #    ebook_tweet = re.sub(r'\s\w+.$','',ebook_tweet)
-        #    print ebook_tweet
+        #randomly drop the last word, as Horse_ebooks appears to do.
+        if random.randint(0,4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
+           print "Losing last word randomly"
+           ebook_tweet = re.sub(r'\s\w+.$','',ebook_tweet)
+           print ebook_tweet
 
-        # #if a tweet is very short, this will randomly add a second sentence to it.
-        # if ebook_tweet != None and len(ebook_tweet) < 40:
-        #     rando = random.randint(0,10)
-        #     if rando == 0 or rando == 7:
-        #         print "Short tweet. Adding another sentence randomly"
-        #         newer_tweet = mine.generate_sentence()
-        #         if newer_tweet != None:
-        #             ebook_tweet += " " + mine.generate_sentence()
-        #         else:
-        #             ebook_tweet = ebook_tweet
-        #     elif rando == 1:
-        #         #say something crazy/prophetic in all caps
-        #         print "ALL THE THINGS"
-        #         ebook_tweet = ebook_tweet.upper()
+        #if a tweet is very short, this will randomly add a second sentence to it.
+        if ebook_tweet != None and len(ebook_tweet) < 40:
+            rando = random.randint(0,10)
+            if rando == 0 or rando == 7:
+                print "Short tweet. Adding another sentence randomly"
+                newer_tweet = mine.generate_sentence()
+                if newer_tweet != None:
+                    ebook_tweet += " " + mine.generate_sentence()
+                else:
+                    ebook_tweet = ebook_tweet
+            elif rando == 1:
+                #say something crazy/prophetic in all caps
+                print "ALL THE THINGS"
+                ebook_tweet = ebook_tweet.upper()
 
-        #throw out tweets that match anything from the source account.
-        # if ebook_tweet != None and len(ebook_tweet) < 110:
-        #     for tweet in source_tweets:
-        #         if ebook_tweet[:-1] not in tweet:
-        #             continue
-        #         else:
-        #             print "TOO SIMILAR: " + ebook_tweet
-        #             sys.exit()
+        throw out tweets that match anything from the source account.
+        if ebook_tweet != None and len(ebook_tweet) < 110:
+            for tweet in source_tweets:
+                if ebook_tweet[:-1] not in tweet:
+                    continue
+                else:
+                    print "TOO SIMILAR: " + ebook_tweet
+                    sys.exit()
 
         if DEBUG == False:
             status = api.PostUpdate(ebook_tweet)
