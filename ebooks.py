e0c44162861cd6b2dd9f62757ffceb05bc33d9de
faooful <faooful@gmail.com>
2016-11-23 17:52:35 +0000

    will did things

--- ebooks.py
+++ ebooks.py
@@ -88,52 +88,59 @@ if __name__=="__main__":
                 if len(source_tweets) == 0:
                     print "Error fetching tweets from Twitter. Aborting."
                     sys.exit()
-        mine = markov.MarkovChainer(order)
-        for tweet in source_tweets:
-            if re.search('([\.\!\?\"\']$)', tweet):
-                pass
-            else:
-                tweet+="."
-            mine.add_text(tweet)
+        # mine = markov.MarkovChainer(order)
+        # for tweet in source_tweets:
+        #     if re.search('([\.\!\?\"\']$)', tweet):
+        #         pass
+        #     else:
+        #         tweet+="."
+        #     mine.add_text(tweet)
 
-        for x in range(0,10):
-            ebook_tweet = mine.generate_sentence()
+        # for x in range(0,10):
+        #     ebook_tweet = mine.generate_sentence()
 
-        #randomly drop the last word, as Horse_ebooks appears to do.
-        if random.randint(0,4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
-           print "Losing last word randomly"
-           ebook_tweet = re.sub(r'\s\w+.$','',ebook_tweet)
-           print ebook_tweet
+        source_length = len(source_tweets)
+        word_one = source_tweets[random(range(source_length))]
+        word_two = None
+        while not word_two and word_two == word_one:
+            word_two = source_tweets[random(range(source_length))]
+        ebook_tweet = " ".join([word_one, word_two])
 
-        #if a tweet is very short, this will randomly add a second sentence to it.
-        if ebook_tweet != None and len(ebook_tweet) < 40:
-            rando = random.randint(0,10)
-            if rando == 0 or rando == 7:
-                print "Short tweet. Adding another sentence randomly"
-                newer_tweet = mine.generate_sentence()
-                if newer_tweet != None:
-                    ebook_tweet += " " + mine.generate_sentence()
-                else:
-                    ebook_tweet = ebook_tweet
-            elif rando == 1:
-                #say something crazy/prophetic in all caps
-                print "ALL THE THINGS"
-                ebook_tweet = ebook_tweet.upper()
+        # #randomly drop the last word, as Horse_ebooks appears to do.
+        # if random.randint(0,4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
+        #    print "Losing last word randomly"
+        #    ebook_tweet = re.sub(r'\s\w+.$','',ebook_tweet)
+        #    print ebook_tweet
+
+        # #if a tweet is very short, this will randomly add a second sentence to it.
+        # if ebook_tweet != None and len(ebook_tweet) < 40:
+        #     rando = random.randint(0,10)
+        #     if rando == 0 or rando == 7:
+        #         print "Short tweet. Adding another sentence randomly"
+        #         newer_tweet = mine.generate_sentence()
+        #         if newer_tweet != None:
+        #             ebook_tweet += " " + mine.generate_sentence()
+        #         else:
+        #             ebook_tweet = ebook_tweet
+        #     elif rando == 1:
+        #         #say something crazy/prophetic in all caps
+        #         print "ALL THE THINGS"
+        #         ebook_tweet = ebook_tweet.upper()
 
         #throw out tweets that match anything from the source account.
-        if ebook_tweet != None and len(ebook_tweet) < 110:
-            for tweet in source_tweets:
-                if ebook_tweet[:-1] not in tweet:
-                    continue
-                else:
-                    print "TOO SIMILAR: " + ebook_tweet
-                    sys.exit()
+        # if ebook_tweet != None and len(ebook_tweet) < 110:
+        #     for tweet in source_tweets:
+        #         if ebook_tweet[:-1] not in tweet:
+        #             continue
+        #         else:
+        #             print "TOO SIMILAR: " + ebook_tweet
+        #             sys.exit()
 
-            if DEBUG == False:
-                status = api.PostUpdate(ebook_tweet)
-                print status.text.encode('utf-8')
-            else:
-                print ebook_tweet
+        if DEBUG == False:
+            status = api.PostUpdate(ebook_tweet)
+            print status.text.encode('utf-8')
+        else:
+            print ebook_tweet
 
         elif ebook_tweet == None:
             print "Tweet is empty, sorry."
