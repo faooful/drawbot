3f484322a8381b6a27bc4fb2e040c4e1abee3bfb
faooful <faooful@gmail.com>
2016-11-23 17:13:04 +0000

    increase the oods

--- local_settings.py
+++ local_settings.py
@@ -9,7 +9,7 @@ MY_ACCESS_TOKEN_KEY = '801369088840892417-kf0kdHQcXJAXb8Av40aKu1FMwyfTIai'
 MY_ACCESS_TOKEN_SECRET = 'wtferkHswd9hr1YkvnQ3qciZyz0LrBsFRcqHYG9JX12dt'
 
 SOURCE_ACCOUNTS = ["faooful"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 0 #How often do you want this to run? 1/8 times?
+ODDS = 1 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = False #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
