1966d8a62c9fb22e7aa21b15f97007f3e2a4e2e0
faooful <faooful@gmail.com>
2016-11-23 14:48:26 +0000

    changed source account

--- local_settings.py
+++ local_settings.py
@@ -8,7 +8,7 @@ MY_CONSUMER_SECRET = 'BWDVorwkzAoOiKxeoU5UtGwpT8aVi8RQVoM7wKxbrTOeQ2tgh4'
 MY_ACCESS_TOKEN_KEY = '801369088840892417-kf0kdHQcXJAXb8Av40aKu1FMwyfTIai'
 MY_ACCESS_TOKEN_SECRET = 'wtferkHswd9hr1YkvnQ3qciZyz0LrBsFRcqHYG9JX12dt'
 
-SOURCE_ACCOUNTS = ["faooful"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 0 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = True #Set this to False to start Tweeting live
