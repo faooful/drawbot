2a494af7afb144d447d80f6fb71e639704db2ab5
faooful <faooful@gmail.com>
2016-11-24 16:51:50 +0000

    working bot through using tweepy and command line

--- README.md
+++
@@ -1,3 +0,0 @@
-# drawbot
-Randomise words for a two minute sketch every day.
-Modifying @tommeagher's [heroku_ebooks](https://github.com/tommeagher/heroku_ebooks).
---
+++ credentials.py
@@ -0,0 +1,12 @@
+# Credentials for your Twitter bot account
+
+# 1. Sign into Twitter or create new account
+# 2. Make sure your mobile number is listed at twitter.com/settings/devices
+# 3. Head to apps.twitter.com and select Keys and Access Tokens
+
+CONSUMER_KEY = 'EafsH6rmlJrgYBqDZBSDHUYc2'
+CONSUMER_SECRET = 'BWDVorwkzAoOiKxeoU5UtGwpT8aVi8RQVoM7wKxbrTOeQ2tgh4'
+
+# Create a new Access Token
+ACCESS_TOKEN = '801369088840892417-kf0kdHQcXJAXb8Av40aKu1FMwyfTIai'
+ACCESS_SECRET = 'wtferkHswd9hr1YkvnQ3qciZyz0LrBsFRcqHYG9JX12dt'
