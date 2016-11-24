c87c20e5053cd1912b942cd36fc9c8e4eedeaf07
faooful <faooful@gmail.com>
2016-11-24 17:58:33 +0000

    removing creds from public view

--- mybot.py
+++ mybot.py
@@ -1,5 +1,12 @@
-import tweepy, time, random
+import tweepy, time, random, os
 from credentials import *
+
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
